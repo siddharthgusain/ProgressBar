8ce11d17b76961504c2cc97430c79dfe46a72fc2
siddharth gusain <siddharthgusain26011999@gmail.com>
2019-06-11 13:10:22 +0530

    changing Fill-color

--- src/components/FillColor/FillColor.js
+++ src/components/FillColor/FillColor.js
@@ -3,15 +3,15 @@ import './FillColor.css';
 
 const FillColor = (props)=>{
     
-    if(props.percent === 20)
+    if(props.percent >0 && props.percent<=20) 
         return <div className="fill-color" style={{width: `${props.percent}%`,background:'red'}} />
-    else if(props.percent === 40)
+    else if(props.percent >20 && props.percent <=40)
         return <div className="fill-color" style={{width: `${props.percent}%`,background:'blue'}} />    
-    else if(props.percent === 60)
+    else if(props.percent > 40 && props.percent<=60)
         return <div className="fill-color" style={{width: `${props.percent}%`,background:'green'}} />
-    else if(props.percent === 80)
+    else if(props.percent > 60 && props.percent<=80)
         return <div className="fill-color" style={{width: `${props.percent}%`,background:'pink'}} />
-    else if(props.percent === 100)
+    else if(props.percent > 80 && props.percent<=100)
         return <div className="fill-color" style={{width: `${props.percent}%`,background:'brown'}} />
 }
 
